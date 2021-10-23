5381e3cb974487ebde999d248b73a7651cb8630a
oleg.romanov <oleg.romanov@apprime.de>
2021-10-23 12:49:01 +0200

    Updates

---
+++ precache-manifest.fe43ee84b3555ad9dbfb383cd980192f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6abc2c9cdfba7168e8f16d4c3189fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52a3359778a53feb1687",
+    "url": "/static/js/2.9b68dcbb.chunk.js"
+  },
+  {
+    "revision": "e46a47a3c9ffc5418527a542926e2a95",
+    "url": "/static/js/2.9b68dcbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965a3162dd2855be0999",
+    "url": "/static/js/main.f29ea878.chunk.js"
+  },
+  {
+    "revision": "f98bb114a8d8f6ac2af8",
+    "url": "/static/js/runtime-main.242c9265.js"
+  }
+]);
